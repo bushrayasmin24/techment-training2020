e0b6b47405a5c3c961a5ab64791bcf38c80d16b5
bushrayasmin24 <36411009+bushrayasmin24@users.noreply.github.com>
2020-05-29 01:35:35 +0530

    Add files via upload

---
+++ day3/Sample.java
@@ -0,0 +1,15 @@
+package com.techment.training.day3;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=5;i++)
+		{
+			sum+=i;
+			System.out.println(i +"sum is "+sum);
+		}
+		System.out.println("Total sum"+sum);
+	}
+
+}
---
+++ day3/Sample2.java
@@ -0,0 +1,16 @@
+package com.techment.training.day3;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		
+	int arr[] = new int[5];
+	for(int i=1;i<5;i++)
+	{
+		arr[i]=i;
+		System.out.println(arr[i]);
+	}
+	
+	}
+
+}
